944927c588e3fae193c6e6b9eacf9052471d8245
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-11 23:51:47 +0200

    Async comprehesions

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async comprehension with Python"""
+import asyncio
+import random
+from typing import Any
+
+
+async def async_generator() -> Any:
+    """async generator"""
+    for _ in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
